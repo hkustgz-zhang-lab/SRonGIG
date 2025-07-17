0e6706647316ff86bcfe3a8a129dea403915f07c
JingrenWang <wjrforcyber@163.com>
2025-07-17 21:56:06 +0800

    Feat(Partition): Extract and write back using window based method
    Signed-off-by: JingrenWang <wjrforcyber@163.com>

--- experiments/reader_simple_partition.cpp
+++ experiments/reader_simple_partition.cpp
@@ -1,6 +1,9 @@
 #include "experiments.hpp"
 #include <_stdio.h>
+#include <cassert>
+#include <cstddef>
 #include <fmt/core.h>
+#include <fmt/format.h>
 #include <lorina/aiger.hpp>
 #include <memory>
 #include <mockturtle/algorithms/klut_to_graph.hpp>
@@ -8,6 +11,7 @@
 #include <mockturtle/io/aiger_reader.hpp>
 #include <mockturtle/networks/aig.hpp>
 #include <mockturtle/views/depth_view.hpp>
+#include <mockturtle/views/partition_view.hpp>
 #include <mtkahypar.h>
 #include <thread>
 
@@ -40,6 +44,20 @@ int creatDir( const std::filesystem::path* dir_path )
   return 0;
 }
 
+std::map<mt_kahypar_hypernode_id_t, mt_kahypar_partition_id_t> convertToMap(
+    const std::unique_ptr<mt_kahypar_partition_id_t[]>& array,
+    size_t size )
+{
+  std::map<mt_kahypar_hypernode_id_t, mt_kahypar_partition_id_t> result;
+  result[0] = -1;
+  for ( size_t i = 0; i < size; ++i )
+  {
+    result[i + 1] = array[i];
+  }
+  assert( result.size() == size + 1 );
+  return result;
+}
+
 int main()
 {
   /*
@@ -64,26 +82,32 @@ int main()
 
   for ( auto const& benchmark : epfl_benchmarks() )
   {
+    if ( benchmark == "hyp" )
+    {
+      continue;
+    }
     fmt::print( "[i] processing {}\n", benchmark );
 
     // TODO: @Jingren Read from mt directly
-    // aig_network aig;
-    // if ( lorina::read_aiger( benchmark_path( benchmark ), aiger_reader( aig ) ) != lorina::return_code::success )
-    //{
-    //   continue;
-    // }
+    aig_network aig;
+    if ( lorina::read_aiger( benchmark_path( benchmark ), aiger_reader( aig ) ) != lorina::return_code::success )
+    {
+      continue;
+    }
 
-    // const uint32_t aig_size_before = aig.num_gates();
-    // const uint32_t aig_depth_before = depth_view( aig ).depth();
+    partition_view_params ps;
+    ps.file_name = fmt::format( "{}/tmp_ctrl.hmetis", pHyOutS );
+    partition_view aig_p{ aig, ps };
 
-    std::string command = fmt::format( "abc -q \"read_aiger {}; write_hmetis {}/tmp.hmetis;\"", benchmark_path( benchmark ), pHyOutS );
-#if WIN32
-    std::unique_ptr<FILE, decltype( &_pclose )> pipe( _popen( command.c_str(), "r" ), _pclose );
-#else
-    // std::unique_ptr<FILE, decltype( &pclose )> pipe( popen( command.c_str(), "r" ), pclose );
-#endif
-    // Use system call here, the above method leads to IO error
-    system( command.c_str() );
+    // Althernative ABC method
+    //    std::string command = fmt::format( "abc -q \"read_aiger {}; write_hmetis {}/tmp.hmetis;\"", benchmark_path( benchmark ), pHyOutS );
+    // #if WIN32
+    //    std::unique_ptr<FILE, decltype( &_pclose )> pipe( _popen( command.c_str(), "r" ), _pclose );
+    // #else
+    //    // std::unique_ptr<FILE, decltype( &pclose )> pipe( popen( command.c_str(), "r" ), pclose );
+    // #endif
+    //    // Use system call here, the above method leads to IO error
+    //    system( command.c_str() );
 
     // use the official repo's example as a test
     mt_kahypar_error_t error{};
@@ -94,11 +118,11 @@ int main()
         true /* activate interleaved NUMA allocation policy */ );
 
     // Setup partitioning context
-    mt_kahypar_context_t* context = mt_kahypar_context_from_preset( DEFAULT );
+    mt_kahypar_context_t* context = mt_kahypar_context_from_preset( DETERMINISTIC );
     // In the following, we partition a hypergraph into two blocks
     // with an allowed imbalance of 3% and optimize the connective metric (KM1)
     mt_kahypar_set_partitioning_parameters( context,
-                                            3 /* number of blocks */, 0.03 /* imbalance parameter */,
+                                            2 /* number of blocks */, 0.03 /* imbalance parameter */,
                                             KM1 /* objective function */ );
     mt_kahypar_set_seed( 42 /* seed */ );
     // Enable logging
@@ -108,7 +132,7 @@ int main()
 
     // Load Hypergraph for DEFAULT preset
     mt_kahypar_hypergraph_t hypergraph =
-        mt_kahypar_read_hypergraph_from_file( fmt::format( "{}/tmp.hmetis", pHyOutS ).c_str(),
+        mt_kahypar_read_hypergraph_from_file( fmt::format( "{}/tmp_ctrl.hmetis", pHyOutS ).c_str(),
                                               context, HMETIS /* file format */, &error );
     if ( hypergraph.hypergraph == nullptr )
     {
@@ -130,8 +154,101 @@ int main()
         mt_kahypar_num_hypernodes( hypergraph ) );
     mt_kahypar_get_partition( partitioned_hg, partition.get() );
 
+    // Iterate over all edges
+    /*
+    The idea is that the connectivity larger than 1 is identified as crossing multiple blocks.
+    Get the boundaries and those nodes connected with boundaries.
+    There's also a problem about the direction, let's hope after the depth view this can be solved easily.
+    */
+
+    std::map<mt_kahypar_hyperedge_id_t, std::vector<mt_kahypar_hypernode_id_t>> vBoundaries;
+    for ( mt_kahypar_hyperedge_id_t edge = 0; edge < mt_kahypar_num_hyperedges( hypergraph ); ++edge )
+    {
+      // std::cout << "Edge " << edge << std::endl;
+      //  Print number of blocks connected by edge
+      mt_kahypar_partition_id_t connectedBlockNum = mt_kahypar_connectivity( partitioned_hg, edge );
+      // std::cout << "Connectivity = " << connectedBlockNum << std::endl;
+      std::vector<mt_kahypar_hypernode_id_t> pins_buffer;
+      pins_buffer.resize( mt_kahypar_hyperedge_size( hypergraph, edge ) );
+      mt_kahypar_get_hyperedge_pins( hypergraph, edge, pins_buffer.data() );
+      // mt_kahypar_get_hyperedge_pins(hypergraph, edge, pins_buffer.get());
+      if ( connectedBlockNum > 1 )
+      {
+        if ( vBoundaries.count( edge ) > 0 )
+        {
+          std::cout << "Encounter with edges twice, this shouldn't happen." << std::endl;
+        }
+        else
+        {
+          vBoundaries[edge] = pins_buffer;
+        }
+      }
+    }
+    std::cout << "Number of the boundaries " << vBoundaries.size() << std::endl;
+    // show the map structure in detail
+    for ( auto& item : vBoundaries )
+    {
+      mt_kahypar_hypernode_id_t hypId = 0;
+      while ( hypId < item.second.size() )
+      {
+        // Recover the original id in hMetis
+        item.second[hypId]++;
+        hypId++;
+      }
+    }
+    std::cout << std::endl;
+
+    // save the result to std::map<nodeId, blockid>
+    std::map<mt_kahypar_hypernode_id_t, mt_kahypar_partition_id_t> node_block_io;
+    std::map<mt_kahypar_hypernode_id_t, mt_kahypar_partition_id_t> node_block;
+    for ( auto& item : vBoundaries )
+    {
+      mt_kahypar_hypernode_id_t hypId = 0;
+      while ( hypId < item.second.size() )
+      {
+        if ( node_block_io.count( item.second[hypId] ) > 0 )
+        {
+          hypId++;
+          continue;
+        }
+        else
+        {
+          node_block_io[item.second[hypId] - 1] = mt_kahypar_block_id( partitioned_hg, item.second[hypId] - 1 );
+          hypId++;
+        }
+      }
+    }
+    std::cout << "Size of all the boundary nodes is " << node_block_io.size() << std::endl;
+    node_block = convertToMap( partition, mt_kahypar_num_hypernodes( hypergraph ) );
+
+    auto vAigs = aig_p.construct_from_partition( 2, vBoundaries, node_block_io, node_block );
+
+    // Now insert all the aigs back to original ntk and check the equivalence
+    // for ( auto& aig_part : vAigs )
+    //{
+    //  // create signals
+    //  std::vector<aig_network::signal> i_sigs;
+    //  for ( auto const& i : std::get<1>( aig_part ) )
+    //  {
+    //    i_sigs.push_back( aig.make_signal( i ) );
+    //  }
+    //  uint32_t counter = 0u;
+    //  // insert back now
+    //  insert_ntk( aig, i_sigs.begin(), i_sigs.end(), std::get<0>( aig_part ), [&]( aig_network::signal const& _new ) {
+    //    auto const _old = std::get<2>( aig_part ).at( counter++ );
+    //    if ( _old != _new )
+    //    {
+    //      aig.substitute_node( aig.get_node( _old ), aig.is_complemented( _old ) ? !_new : _new );
+    //    }
+    //  } );
+    //}
+
+    const auto cec1 = benchmark == "hyp" ? true : abc_cec( aig, benchmark );
+    assert( cec1 == true );
+    std::cout << "*****EQ CHEKCED*****" << std::endl;
+
     // Extract Block Weights
-    auto block_weights = std::make_unique<mt_kahypar_hypernode_weight_t[]>( 3 );
+    auto block_weights = std::make_unique<mt_kahypar_hypernode_weight_t[]>( 2 );
     mt_kahypar_get_block_weights( partitioned_hg, block_weights.get() );
 
     // Compute Metrics
@@ -139,12 +256,12 @@ int main()
     const int km1 = mt_kahypar_km1( partitioned_hg );
 
     // Output Results
-    std::cout << "Partitioning Results:" << std::endl;
+    std::cout << "====================\nPartitioning Results:" << std::endl;
     std::cout << "Imbalance         = " << imbalance << std::endl;
     std::cout << "Km1               = " << km1 << std::endl;
     std::cout << "Weight of Block 0 = " << block_weights[0] << std::endl;
     std::cout << "Weight of Block 1 = " << block_weights[1] << std::endl;
-    std::cout << "Weight of Block 2 = " << block_weights[2] << std::endl;
+    // std::cout << "Weight of Block 2 = " << block_weights[2] << std::endl;
     std::cout << std::endl;
 
     // You cannot give a directory that doesn't exist here, std::ofstream doesn't create new directory by default
@@ -152,14 +269,18 @@ int main()
                                                                          fmt::format( "{}/{}hypOut", pHyOutS, benchmark ).c_str(),
                                                                          &error );
     assert( status_out == SUCCESS );
-    if ( remove( fmt::format( "{}/tmp.hmetis", pHyOutS ).c_str() ) == 0 )
-    {
-      std::cout << "Successfully deleted the tmp file." << std::endl;
-    }
-    else
-    {
-      std::cout << "Failed to delete the tmp file." << std::endl;
-    }
+
+    // alternative without file write out, using:
+    // mt_kahypar_partitioned_hypergraph_t partitioned_hg_sep = mt_kahypar_create_partitioned_hypergraph(hypergraph, context, 3, partition.get(), &error);
+
+    // if ( remove( fmt::format( "{}/tmp.hmetis", pHyOutS ).c_str() ) == 0 )
+    //{
+    //   std::cout << "Successfully deleted the tmp file." << std::endl;
+    // }
+    // else
+    //{
+    //   std::cout << "Failed to delete the tmp file." << std::endl;
+    // }
 
     mt_kahypar_free_context( context );
     mt_kahypar_free_hypergraph( hypergraph );
--- include/mockturtle/views/partition_view.hpp
+++ include/mockturtle/views/partition_view.hpp
@@ -6,10 +6,17 @@
 */
 
 #include "fanout_view.hpp"
+#include <cassert>
 #include <cstddef>
 #include <fstream>
 #include <iostream>
 #include <mockturtle/networks/aig.hpp>
+#include <mockturtle/traits.hpp>
+#include <mockturtle/utils/network_utils.hpp>
+#include <mockturtle/views/depth_view.hpp>
+#include <ostream>
+#include <string>
+#include <vector>
 
 namespace mockturtle
 {
@@ -32,8 +39,11 @@ public:
   using storage = typename aig_network::storage;
   using node = typename aig_network::node;
   using signal = typename aig_network::signal;
+  using node_id_k = unsigned long int;
+  using block_id = int;
+  using edge_id = unsigned long int;
 
-  explicit partition_view( aig_network const& ntk, partition_view_params const& ps = {} ) : _hypNum( 0 ), _ps( ps )
+  explicit partition_view( aig_network const& ntk, partition_view_params const& ps = {} ) : _hypNum( 0 ), _ps( ps ), _ntk( ntk )
   {
     collect_hypgraph( ntk );
     write_hypgraph( ntk, ps );
@@ -43,7 +53,206 @@ public:
   {
   }
 
+  std::vector<std::tuple<aig_network, std::vector<node>, std::vector<signal>>> construct_from_partition( int nPart, std::map<edge_id, std::vector<node_id_k>> vBoundaries, std::map<node_id_k, block_id> const& node_block_io, std::map<node_id_k, block_id>& node_block )
+  {
+    // create nPart aig_network in parallel
+    std::vector<std::tuple<aig_network, std::vector<node>, std::vector<signal>>> vAigs_win( nPart );
+    // construct aig from scratch
+    std::cout << "Size of the io map is " << node_block_io.size() << std::endl;
+    /*
+    block0: vCis; vCos; vNodes;
+    block1: vCis; vCos; vNodes;
+    ...
+    */
+    std::vector<std::vector<node>> node_id_split( 3 * nPart );
+
+    /*
+    For all pis/pos, these can only be split, even after patition, they are still pis/pos ,don't need to check the boundaries, just might belongs to different block.
+    */
+    _ntk.foreach_pi( [&]( auto const& n_pi ) {
+      auto pi_index = _ntk.node_to_index( n_pi );
+      auto it = node_block.find( pi_index );
+      if ( it == node_block.end() )
+      {
+        std::cout << "Something wrong. There's no PI index " << pi_index << std::endl;
+      }
+      else
+      {
+        auto block_id = it->second;
+        node_id_split[block_id * 3].push_back( n_pi );
+      }
+    } );
+    _ntk.foreach_po( [&]( auto const& n_po ) {
+      auto po_index = _ntk.node_to_index( _ntk.get_node( n_po ) );
+      // should be one of the and type or PI type
+      assert( _ntk.is_and( _ntk.get_node( n_po ) ) || _ntk.is_pi( _ntk.get_node( n_po ) ) || _ntk.is_constant( _ntk.get_node( n_po ) ) );
+      if ( po_index == 0 )
+      {
+        po_index = _ntk.num_gates() + _ntk.num_pis();
+      }
+      auto it = node_block.find( po_index );
+      if ( it == node_block.end() )
+      {
+        std::cout << "Something wrong. There's no PO index " << po_index << std::endl;
+      }
+      else
+      {
+        auto block_id = it->second;
+        node_id_split[block_id * 3 + 1].push_back( _ntk.get_node( n_po ) );
+      }
+    } );
+
+    /*
+    For each boundary, we should identify the source and sinks:
+    There's always a SINGLE source and maybe multiple sinks, this is by design of the AIG, so for a group of nodes attached to a single hyperEdge, the one has the lowest level is the SOURCE.
+    */
+    depth_view<aig_network> ntk_dep{ _ntk };
+    for ( auto& edg : vBoundaries )
+    {
+      assert( edg.second.size() != 0 );
+      auto src_index = edg.second[0];
+      if ( src_index == ( _ntk.num_gates() + _ntk.num_pis() ) )
+      {
+        src_index = 0;
+      }
+      auto node_source = _ntk.index_to_node( src_index );
+      auto index = 1;
+      while ( index < edg.second.size() )
+      {
+        auto target_to_compare = edg.second[index];
+        if ( target_to_compare == _ntk.num_gates() + _ntk.num_pis() )
+        {
+          assert( target_to_compare == _ntk.num_gates() + _ntk.num_pis() );
+          std::cout << "Encounter with const0 id " << target_to_compare << std::endl;
+          target_to_compare = 0;
+        }
+        if ( ntk_dep.level( _ntk.index_to_node( target_to_compare ) ) < ntk_dep.level( node_source ) )
+        {
+          node_source = _ntk.index_to_node( target_to_compare );
+        }
+        index++;
+      }
+      // Add SOURCE to the fanout of the block itself belongs to
+      auto b_s = _ntk.node_to_index( node_source );
+      if ( b_s == 0 )
+      {
+        b_s = _ntk.num_gates() + _ntk.num_pis();
+      }
+      auto& block_id_s = node_block[b_s];
+      auto& ac_pos = node_id_split[block_id_s * 3 + 1];
+      // only single output is needed(?)
+      if ( !check_node_exist( node_id_split[block_id_s * 3 + 1], node_source ) )
+      {
+        ac_pos.push_back( node_source );
+      }
+
+      // SOURCE now is node_source
+      /*
+      Use fanout iteration method, check:
+      The number of different id block in fanouts of the SOURCE show how many CI should be provided for the according block.
+      */
+      index = 0;
+      while ( index < edg.second.size() )
+      {
+        auto it = node_block.find( edg.second[index] );
+        // if exist and not the source of the hyperEdge
+        if ( it != node_block.end() && edg.second[index] != _ntk.node_to_index( node_source ) )
+        {
+          // if node in fanout has different block id as SOURCE, then add SOURCE to CI of the fanout
+          auto tmp_fo_block = node_block[edg.second[index]];
+          if ( tmp_fo_block != node_block[_ntk.node_to_index( node_source )] )
+          {
+            // check if the block already conatins the CI as node_source
+            auto& ac_pis = node_id_split[tmp_fo_block * 3];
+            if ( check_node_exist( ac_pis, _ntk.index_to_node( node_source ) ) )
+            {
+              index++;
+              continue;
+            }
+            else
+            {
+              ac_pis.push_back( _ntk.index_to_node( node_source ) );
+            }
+          }
+          index++;
+        }
+        else
+        {
+          index++;
+          continue;
+        }
+      }
+    }
+
+    auto ori_num_gate = _ntk.num_gates();
+    std::cout << "Num of gate in original ntk : " << ori_num_gate << std::endl;
+    _ntk.foreach_gate( [&]( auto const& n_and ) {
+      auto and_index = _ntk.node_to_index( n_and );
+      auto block_id_and = node_block[and_index];
+      auto cis_id = node_id_split[block_id_and * 3];
+      // Do not contain PIs, as suggested by clone_subnetwork api
+      if ( !check_node_exist( cis_id, n_and ) )
+      {
+        node_id_split[block_id_and * 3 + 2].push_back( n_and );
+      }
+    } );
+    std::cout << "Writing out aigs..." << std::endl;
+    /*
+    Use window based method to construct a subnetwork and each one of them can be insert back to original aig network and maintain equivalence
+    */
+    auto count_all_gate = 0;
+    for ( auto i = 0; i < nPart; i++ )
+    {
+      std::cout << "================\nBlock " << i << std::endl;
+      aig_network win;
+      std::vector<node> inputs = node_id_split[i * 3];
+      assert( inputs.size() > 0 );
+      std::cout << "Size of CI: " << inputs.size() << std::endl;
+      std::vector<node> outputs_o = node_id_split[i * 3 + 1];
+      assert( outputs_o.size() > 0 );
+      std::vector<signal> outputs;
+      construct_out_sig( outputs_o, outputs );
+      std::cout << "Size of CO: " << outputs.size() << std::endl;
+      std::vector<node> gates = node_id_split[i * 3 + 2];
+      std::cout << "Size of gates: " << gates.size() << std::endl;
+      count_all_gate += gates.size();
+      clone_subnetwork( _ntk, inputs, outputs, gates, win );
+      std::get<0>( vAigs_win[i] ) = win;
+      std::get<1>( vAigs_win[i] ) = inputs;
+      std::get<2>( vAigs_win[i] ) = outputs;
+    }
+    assert( count_all_gate == ori_num_gate );
+    return vAigs_win;
+  }
+
 private:
+  void construct_out_sig( std::vector<node> const& outputs_o, std::vector<signal>& outputs )
+  {
+
+    for ( auto& outs_o : outputs_o )
+    {
+      _ntk.foreach_node( [&]( auto const& n ) {
+        _ntk.foreach_fanin( n, [&]( auto const& f ) {
+          if ( _ntk.get_node( f ) == outs_o )
+          {
+            outputs.push_back( _ntk.make_signal( outs_o ) );
+          }
+        } );
+      } );
+    }
+  }
+  bool check_node_exist( std::vector<node> const& nodes, node const& p )
+  {
+    for ( auto& n : nodes )
+    {
+      if ( n == p )
+      {
+        return true;
+      }
+    }
+    return false;
+  }
+
   void collect_single_node( fanout_view<aig_network> const& ntk, node const& n )
   {
     _sinkHyp[n] = {};
@@ -117,6 +326,7 @@ private:
   int _hypNum;
   partition_view_params _ps;
   std::map<node, std::vector<node>> _sinkHyp;
+  aig_network _ntk;
 };
 
 } // namespace mockturtle
